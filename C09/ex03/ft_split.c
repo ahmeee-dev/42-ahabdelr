4f32cf2bf7159bf19b1c21503f628fac72352687
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2024-11-25 23:44:08 +0100

    da casa

--- C09/ex03/ft_split.c
+++ C09/ex03/ft_split.c
@@ -18,7 +18,7 @@ int	is_sep(char c, char *charset)
 	int	i;
 
 	i = 0;
-	while (chrset[i])
+	while (charset[i])
 		{
 			if (c == charset[i])
 				return (1);
@@ -38,22 +38,49 @@ void	ft_position(char *str, char **dest, char *charset)
         i = 0;
         while (str[i] != '\0')
         {
-                if (is_sep(str[i], charset) == 1 && count != 0)
-		{
-			malloc
-			count = 0;
-			dest_count++;
-		}
-                else if (str[i+1] == '\0' && count != 0)
-		{
-			malloc
-			count = 0;
-			dest_count++; //per andare avanti nella dest
-		}
-                else if (is_sep(str[i], charset) == 0)
-                        count++;
-                i++;
+        	if (is_sep(str[i], charset) == 1 && count != 0)
+			{
+				dest[dest_count] = (char *)malloc(count + 1);
+				count = 0;
+				dest_count++;
+			}
+        	else if (str[i+1] == '\0' && count != 0)
+			{
+				dest[dest_count] = (char *)malloc(count + 1);
+				count = 0;
+				dest_count++; 
+			}
+            else if (is_sep(str[i], charset) == 0)
+                count++;
+            i++;
         }
+}
+
+void	putstr(char *str, char *charset, char **dest)
+{
+	int		i;
+	int		count;
+	int		j;
+
+	j = 0;
+	i = 0;
+	count = 0;
+	while (str[j] != 0)
+	{
+		if (is_sep(str[j], charset) == 1 && count != 0)
+		{
+			dest[i][count] = '\0';
+			count = 0;
+			i++;
+		}
+		else if (is_sep(str[j], charset) == 0)
+		{
+			dest[i][count] = str[j];
+			count++;
+		}
+		j++;
+	}
+	*dest[i] = '\0';
 }
 
 char	**ft_split(char *str, char *charset)
@@ -74,11 +101,15 @@ char	**ft_split(char *str, char *charset)
 			count = 0;
 		}
 		else if (str[i+1] == '\0' && count != 0)
+		{
+			count++;
 			words++;
+		}
 		else if (is_sep(str[i], charset) == 0)
 			count++;
 		i++;
 	}
 	*dest = (char *)malloc(words+1);
 	ft_position(str, dest, charset);
+	putstr(str, charset, dest);
 }
