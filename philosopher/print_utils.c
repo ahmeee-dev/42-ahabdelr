980bc52dc783d027dbb468e760858bfa46272a5b
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-03-01 00:50:26 +0100

    feat: implemented an easier logic, based on the n -> n + 1 -> n + 2 (...) logic

--- philosopher/print_utils.c
+++ philosopher/print_utils.c
@@ -6,7 +6,7 @@
 /*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 09:59:20 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/02/27 15:44:56 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/02/28 21:43:47 by ahabdelr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,14 @@ void	print_death(t_philo *philo)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	printf("%lld %d died\n", (((long long)tv.tv_sec * 1000)
-			+ ((long long)tv.tv_usec / 1000)), philo->number);
+	//pthread_mutex_lock(&philo->data->mutex);
+	if (philo->data->alive == 1)
+	{
+		printf("%lld %d died\n", (((long long)tv.tv_sec * 1000)
+				+ ((long long)tv.tv_usec / 1000)), philo->number);
+	}
+	philo->data->alive = 0;
+	//pthread_mutex_unlock(&philo->data->mutex);
 }
 
 void	print_eat(t_philo *philo)
@@ -30,8 +36,13 @@ void	print_eat(t_philo *philo)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
+	//pthread_mutex_lock(&philo->data->mutex);
+	if (philo->data->alive == 1)
+	{
 	printf("%lld %d is eating\n", (((long long)tv.tv_sec * 1000)
 			+ ((long long)tv.tv_usec / 1000)), philo->number);
+	}
+	//pthread_mutex_unlock(&philo->data->mutex);
 	philo->ate = ((long long)tv.tv_sec * 1000) + ((long long)tv.tv_usec / 1000);
 }
 
@@ -40,8 +51,13 @@ void	print_sleep(t_philo *philo)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	printf("%lld %d is sleeping\n", (((long long)tv.tv_sec * 1000)
-			+ ((long long)tv.tv_usec / 1000)), philo->number);
+	//pthread_mutex_lock(&philo->data->mutex);
+	if (philo->data->alive == 1)
+	{
+		printf("%lld %d is sleeping\n", (((long long)tv.tv_sec * 1000)
+				+ ((long long)tv.tv_usec / 1000)), philo->number);
+	}
+	//pthread_mutex_unlock(&philo->data->mutex);
 }
 
 void	print_think(t_philo *philo)
@@ -49,8 +65,13 @@ void	print_think(t_philo *philo)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	printf("%lld %d is thinking\n", (((long long)tv.tv_sec * 1000)
-			+ ((long long)tv.tv_usec / 1000)), philo->number);
+	//pthread_mutex_lock(&philo->data->mutex);
+	if (philo->data->alive == 1)
+	{
+		printf("%lld %d is thinking\n", (((long long)tv.tv_sec * 1000)
+				+ ((long long)tv.tv_usec / 1000)), philo->number);
+	}
+	//pthread_mutex_unlock(&philo->data->mutex);
 }
 
 void	print_fork(t_philo *philo)
@@ -58,6 +79,11 @@ void	print_fork(t_philo *philo)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	printf("%lld %d has taken a fork\n", (((long long)tv.tv_sec * 1000)
-			+ ((long long)tv.tv_usec / 1000)), philo->number);
+	pthread_mutex_lock(&philo->data->mutex);
+	if (philo->data->alive == 1)
+	{
+		printf("%lld %d has taken a fork\n", (((long long)tv.tv_sec * 1000)
+				+ ((long long)tv.tv_usec / 1000)), philo->number);
+	}
+	pthread_mutex_unlock(&philo->data->mutex);
 }
--- philosopher/routine_utils.c
+++ philosopher/routine_utils.c
@@ -6,7 +6,7 @@
 /*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 11:19:13 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/02/27 15:46:12 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/02/28 23:37:14 by ahabdelr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+//il problema si verifica quando i filosofi si dispongono in un ordine "strano"
+// vale a dire che invece di andare 1, 3, 5 ecc... per poi andare con i pari
+//  si va 1, 2, 3 ecc... con i threads che aspettano finisca uno per mandare l'altro
 void	eat_init(t_philo *philo, t_eat *eat)
 {
 	eat->phi_num = philo->data->ph_number;
@@ -23,11 +26,11 @@ void	eat_init(t_philo *philo, t_eat *eat)
 	if (philo->number == 1)
 		eat->prev = eat->phi_num - 1;
 	else
-		eat->prev = eat->actual - 1;
+		eat->prev = - 1;
 	if (philo->number == eat->phi_num)
 		eat->next = 1 - eat->phi_num;
 	else
-		eat->next = philo->number + 1;
+		eat->next = + 1;
 }
 
 int	check_death(t_philo *philo)
@@ -39,10 +42,10 @@ int	check_death(t_philo *philo)
 	gettimeofday(&tv, NULL);
 	time = ((long long)tv.tv_sec * 1000) + ((long long)tv.tv_usec / 1000);
 	last_meal = time - philo->ate;
+	printf("last meal of %d was %dms ago on %d\n", philo->number, last_meal, philo->data->to_die);
 	if (last_meal > philo->data->to_die)
 	{
 		print_death(philo);
-		philo->data->alive = 0;
 		return (1);
 	}
 	return (0);
@@ -60,15 +63,17 @@ int	eat(t_philo *philo)
 	t_eat	eat;
 
 	eat_init(philo, &eat);
+	pthread_mutex_lock(&philo->data->check);
+	pthread_mutex_lock(&((philo)->mutex));
+	print_fork(philo);
 	pthread_mutex_lock(&((philo + eat.next)->mutex));
 	print_fork(philo);
-	pthread_mutex_lock(&((philo + eat.prev)->mutex));
-	print_fork(philo);
+	pthread_mutex_unlock(&philo->data->check);
 	if (check_death(philo))
 		return (0);
 	print_eat(philo);
 	usleep(philo->data->to_eat);
-	pthread_mutex_unlock(&((philo + eat.prev)->mutex));
 	pthread_mutex_unlock(&((philo + eat.next)->mutex));
+	pthread_mutex_unlock(&((philo)->mutex));
 	return (1);
 }
