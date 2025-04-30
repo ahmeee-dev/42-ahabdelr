f473e0d147d45391735a720a849c547f5fe7e6ac
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-04-30 17:09:25 +0200

    commented hints

--- c++_modules/c++00/ex02/Account.cpp
+++ c++_modules/c++00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:32:21 by ahabdelr          #+#    #+#             */
-/*   Updated: 2025/04/30 16:29:38 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/04/30 17:09:04 by ahabdelr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,6 @@
 
 Account::Account(int initial_deposit){
     //gli static sono varibili "condivise" tra le istanze
+    //quindi l'indice può essere caloclato tramite lo scorrere autonomo della creazione di un vettore
+
 }
