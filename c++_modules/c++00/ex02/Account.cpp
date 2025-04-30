f0aa5f1110cba62eda1fc2ec3bfd48f2e4c88daf
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-04-30 16:29:56 +0200

    started ex02

---
+++ c++_modules/c++00/ex02/Account.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 11:32:21 by ahabdelr          #+#    #+#             */
+/*   Updated: 2025/04/30 16:29:38 by ahabdelr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit){
+    //gli static sono varibili "condivise" tra le istanze
+}
