86739352e82698ca016e71117b9a9977017cbabf
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-06-10 16:25:35 +0200

    completed the exercise

---
+++ c++_modules/c++03/ex01/ScravTrap.cpp
@@ -0,0 +1,44 @@
+#include "ScravTrap.hpp"
+
+void ScravTrap::attack() {
+	std::cout << "The earth is shaking, an attack of " << this->attackDamage << " damage has been thrown by " << this->name << std::endl;
+}
+
+ScravTrap::ScravTrap() : ClapTrap() {
+	this->hitPoints = 100;
+	this->name = "";
+	this->attackDamage = 20;
+	this->energyPoints = 50;
+
+	std::cout << "	Bogos binted" << std::endl;
+}
+
+ScravTrap::ScravTrap(ScravTrap &toCopy) : ClapTrap(toCopy) {
+	std::cout << "The Copy of a warrior has risen under the name of " << name << std::endl;
+}
+
+ScravTrap::ScravTrap(std::string name) : ClapTrap(name) {
+	this->hitPoints = 100;
+	this->name = name;
+	this->attackDamage = 20;
+	this->energyPoints = 50;
+
+	std::cout << "	Bogos binted" << std::endl;
+}
+
+ScravTrap::~ScravTrap() {
+	std::cout << this->name << "Has been destroyed by malicious forces" << std::endl;
+}
+
+void ScravTrap::guardGate() {
+	std::cout << "Attack is for battles, defense in for was, and " << this->name << " knows it and goes into GateKeeper Mode" << std::endl;
+}
+
+
+ScravTrap& ScravTrap::operator= (ScravTrap &rhs) {
+	if (this != &rhs)
+	{
+		this->name = rhs.name;
+	}
+	return (*this);
+}
---
+++ c++_modules/c++03/ex01/ScravTrap.hpp
@@ -0,0 +1,12 @@
+#include "ClapTrap.hpp"
+
+class ScravTrap : public ClapTrap {
+	public:
+		ScravTrap();
+		ScravTrap(std::string name);
+		ScravTrap(ScravTrap &toCopy);
+		~ScravTrap();
+		ScravTrap &operator =(ScravTrap &rhs);
+		void guardGate();
+		void attack();
+};
