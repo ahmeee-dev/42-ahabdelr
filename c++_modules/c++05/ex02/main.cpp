9fa7bc76a0c0c2760b72a2cb39b0c3f404747485
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-07-31 14:19:23 +0200

    feat: completed ex02

--- c++_modules/c++05/ex02/main.cpp
+++ c++_modules/c++05/ex02/main.cpp
@@ -2,12 +2,14 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main(void) {
 
 	Bureaucrat mario("Mario Draghi", 2);
 	ShrubberyCreationForm bush("bush", "fire");
 	RobotomyRequestForm robo("Robot", "Eusebio");
+	PresidentialPardonForm pardon("Zarbon", "Darbula");
 
 	//std::cout << mario.getGrade() << std::endl;
 	//mario.signForm(bush);
@@ -15,8 +17,11 @@ int main(void) {
 	std::cout << mario.getGrade() << std::endl;;
 	mario.signForm(bush);
 	mario.signForm(robo);
+	mario.signForm(pardon);
+	mario.executeForm(pardon);
 	mario.executeForm(robo);
 	mario.executeForm(bush);
+	
 
 	std::cout << "E dopo questo Mario si ritira" << std::endl;
 
